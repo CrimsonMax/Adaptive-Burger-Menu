317801aca2a36b8994b96607ddbda410db8dcdf1
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2020-08-05 02:03:22 +0300

    pop

--- site.js
+++ site.js
@@ -4,18 +4,31 @@ for (i = 0; i < clickedElem.length; i++) {
     clickedElem[i].addEventListener('click', show);
 }
 
-let target = document.querySelectorAll('.subElem');
+// let target = document.querySelectorAll('.subElem');
 
-for (i = 0; i < target.length; i++) {
-    target[i].addEventListener('click', show);
-}
+// for (i = 0; i < target.length; i++) {
+//     target[i].addEventListener('click', show);
+// }
 
 function toggle() {
     document.querySelector(".container").classList.toggle("show");
 }
 
 function show() {
-    document.querySelector(".subElem").classList.toggle("show2");
+    // let choosenOption = document.querySelectorAll(".subElem");
+    // let setOption = oneOption.style.display == "flex";
+    let oneOption = document.querySelector(".subElem");
+    oneOption.classList.toggle("show2");
+    // for (i = 0; i < choosenOption.length; i++) {
+    //     if (!setOption) {
+            
+    //     }
+    // }
+        // document.querySelector(".subElem").
 }
 
+// classList.toggle("show2");
+
 document.querySelector('.mobileMenu').addEventListener('click', toggle);
+
+// document.querySelector('.options').addEventListener('click', appears);
