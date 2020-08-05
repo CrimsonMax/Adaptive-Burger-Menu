878612189f8715e27254afe727866c587e051067
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2020-08-05 22:26:42 +0300

    Done!

--- site.js
+++ site.js
@@ -1,34 +1,21 @@
-let clickedElem = document.querySelectorAll('.elem');
+let clickedElem = document.querySelectorAll('.options');
 
 for (i = 0; i < clickedElem.length; i++) {
-    clickedElem[i].addEventListener('click', show);
+    let target = clickedElem[i].nextElementSibling;
+    clickedElem[i].addEventListener('click', function () {
+        let notNeed = document.querySelectorAll(".subElem");
+        for (j = 0; j < notNeed.length; j++) {
+            if (not[j].classList.contains("show2")) {
+                not[j].classList.remove("show2");
+            } else {
+                target.classList.toggle("show2");
+            }
+        }
+    });
 }
-
-// let target = document.querySelectorAll('.subElem');
-
-// for (i = 0; i < target.length; i++) {
-//     target[i].addEventListener('click', show);
-// }
 
 function toggle() {
     document.querySelector(".container").classList.toggle("show");
 }
 
-function show() {
-    // let choosenOption = document.querySelectorAll(".subElem");
-    // let setOption = oneOption.style.display == "flex";
-    let oneOption = document.querySelector(".subElem");
-    oneOption.classList.toggle("show2");
-    // for (i = 0; i < choosenOption.length; i++) {
-    //     if (!setOption) {
-            
-    //     }
-    // }
-        // document.querySelector(".subElem").
-}
-
-// classList.toggle("show2");
-
 document.querySelector('.mobileMenu').addEventListener('click', toggle);
-
-// document.querySelector('.options').addEventListener('click', appears);
